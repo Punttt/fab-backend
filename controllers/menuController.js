11bfecc57f935482d2676f78ec4428246a1b9746
Pontus Johansson <johanssonp998@gmail.com>
2026-05-25 12:27:48 +0200

    börjar med validering för fälten och gräns för vecka

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -3,8 +3,26 @@
 const menuModel = require("../models/menuModel");
 
 // Skapar en ny veckomenu 
-const createMenu = async (requestAnimationFrame, res) => {
+const createMenu = async (req, res) => {
     try {
+        const { week_number, year } = req.body;
+
+        // Validerar att fälten finns.
+        if (!week_number || !year) {
+            return res.status(400).json({
+                error: "Vecka och år krävs"
+            });
+        }
+
+        // Valdierar att veckanummer är inom 1-52
+        if (week_number < 1 || week_number > 52) {
+            return res.status(400).json({
+                error: "Vecka måste vara mellan 1-52"
+            });
+        }
+
+        // Validerar för att kontrllera att veckan inte finns i db
+        const existing = await menuModel.findWeekMenu(week_number, year)
 
     } catch (error) {
         console.error(error);
