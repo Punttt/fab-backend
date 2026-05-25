2eb6adc8a94ee9bde0183d85b9519b0650fc79c5
Pontus Johansson <johanssonp998@gmail.com>
2026-05-25 14:43:28 +0200

    kopplar samman routes

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -1,10 +1,20 @@
 // Routes 
 const express = require("express");
 const router = express.Router();
-const { createMenu } = require("../controllers/menuController");
+const { 
+    createMenu,
+    addMenuItem,
+    editMenuItem,
+    deletedItem
+} = require("../controllers/menuController");
 const authenticate = require("../middleware/authMiddleware");
 
 // POST 
 router.post("/", authenticate, createMenu);
+
+// Menyrätter
+router.post("/item", authenticate, addMenuItem);
+router.put("/item/:id", authenticate, editMenuItem);
+router.delete("/item/:id", authenticate, deletedItem);
 
 module.exports = router;
