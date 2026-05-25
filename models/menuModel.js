d54e78e1277809d1e1da2a61f8e3c2835376c992
Pontus Johansson <johanssonp998@gmail.com>
2026-05-25 13:55:31 +0200

    skapa en funktion för att skapa en rätt

--- models/menuModel.js
+++ models/menuModel.js
@@ -1,6 +1,7 @@
 
 const pool = require('./db');
 
+// VECKOMENY
 // Skapar ny veckomeny
 async function createWeeklyMenu(weekNumber, year, createdBy) {
     const result = await pool.query(
@@ -20,6 +21,18 @@ async function findWeekMenu(weekNumber, year) {
         [weekNumber, year]
     );
 
+    return result.rows[0];
+}
+
+// DAILYRÄTT
+// Skapa en menyrätt för en specifik dag
+async function createMenuItem(weeklyMenuId, dayOfWeek, dish) {
+    const result = await pool.query(
+        `INSERT INTO menu_items (weekly_menu_id, day_of_week_ dish)
+        VALUES($1, $2, $3)
+        RETURNING *`,
+        [weeklyMenuId, dayOfWeek, dish]
+    );
     return result.rows[0];
 }
 
